dec4fb5915164157b3d3c4029c79fba383959964
toxpuro <puro.touko2@gmail.com>
2021-11-22 22:39:10 +0200

    push to github

--- src/main.py
+++ src/main.py
@@ -144,7 +144,7 @@ def generate_initial_state2():
 def load_images():
     pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bR", "bN", "bB", "bK", "bQ"]
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load(f"../images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load(f"./images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
 
 
 def start_the_game(gs=None):
